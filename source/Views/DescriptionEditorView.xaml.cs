d640f5b2cd23d8ee39671c80f58760104b2d8f6a
Lacro59 <lacro59@gmail.com>
2025-12-25 12:27:13 +0100

    Refactor DescriptionEditorView UI and code structure
    Redesigned the DescriptionEditorView toolbar and bottom action buttons for improved layout and maintainability. Replaced hardcoded button and context menu definitions with reusable styles and resource-based menus in XAML. In the code-behind, unified and simplified event handlers, renamed controls for clarity, and modularized logic for image insertion and undo/redo functionality. Improved selection highlighting and streamlined HTML/Markdown formatting actions.

--- source/Views/DescriptionEditorView.xaml.cs
+++ source/Views/DescriptionEditorView.xaml.cs
@@ -2,7 +2,6 @@
 using Playnite.SDK;
 using CommonPluginsShared;
 using System;
-using System.Linq;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using System.Windows;
@@ -14,23 +13,19 @@ using System.Windows.Input;
 
 namespace DescriptionEditor.Views
 {
-    /// <summary>
-    /// Logique d'interaction pour DescriptionEditorView.xaml
-    /// </summary>
     public partial class DescriptionEditorView : UserControl
     {
         private static IResourceProvider resources { get; set; } = new ResourceProvider();
 
         public string Description { get; set; }
         private TextBox _TextDescription { get; set; }
-
         private HtmlTextView htmlTextView { get; set; } = new HtmlTextView();
-
 
         private bool DisableEvent { get; set; } = false;
         private int IndexUndo { get; set; } = 0;
         private List<string> ListUndo { get; set; } = new List<string>();
 
+        #region Constructor
 
         public DescriptionEditorView(TextBox TextDescription)
         {
@@ -41,8 +36,6 @@ namespace DescriptionEditor.Views
 
             PlayniteTools.SetThemeInformation();
             string DescriptionViewFile = ThemeFile.GetFilePath("DescriptionView.html");
-
-            Common.LogDebug(true, $"{DescriptionViewFile}");
 
             try
             {
@@ -55,7 +48,7 @@ namespace DescriptionEditor.Views
                 htmlTextView.HtmlForeground = (Color)resources.GetResource("TextColor");
                 htmlTextView.LinkForeground = (Color)resources.GetResource("GlyphColor");
 
-                PART_HtmlDescription.Children.Add(htmlTextView);
+                _ = HtmlPreviewPanel.Children.Add(htmlTextView);
             }
             catch (Exception ex)
             {
@@ -65,6 +58,9 @@ namespace DescriptionEditor.Views
             DataContext = this;
         }
 
+        #endregion
+
+        #region Window Actions
 
         private void BtEditorCancel_Click(object sender, RoutedEventArgs e)
         {
@@ -73,300 +69,187 @@ namespace DescriptionEditor.Views
 
         private void BtEditorOK_Click(object sender, RoutedEventArgs e)
         {
-            _TextDescription.Text = DescriptionActual.Text;
+            _TextDescription.Text = DescriptionTextBox.Text;
             ((Window)this.Parent).Close();
         }
 
-
-        #region Common formatter 
-        private void BtHtmlFormat_Click(object sender, RoutedEventArgs e)
-        {
-            DescriptionActual.Text = HtmlHelper.HtmlFormat(DescriptionActual.Text);
-        }
-
-
-        private void BtHtmlFormatRemove_Click(object sender, RoutedEventArgs e)
-        {
-            DescriptionActual.Text = HtmlHelper.HtmlFormatRemove(DescriptionActual.Text);
-        }
-
-        private void BtMarkdownToHtml_Click(object sender, RoutedEventArgs e)
-        {
-            DescriptionActual.Text = HtmlHelper.MarkdownToHtml(DescriptionActual.Text);
-        }
-
-        private string RemoveParagraph(string Text)
-        {
-            if (Text.Substring(Text.Length - 1, 1) == "\n")
-            {
-                Text = Text.Substring(0, Text.Length - 1);
-            }
-
-            string StartString = Text.Substring(0, 3).ToLower();
-            string EndString = Text.Substring(Text.Length - 4, 4).ToLower();
-
-            if (StartString == "<p>" && EndString == "</p>")
-            {
-                Text = Text.Substring(3, Text.Length - 3);
-                Text = Text.Substring(0, Text.Length - 4);
-            }
-
-            return Text;
-        }
-
-
-        private void BtHtoB_Click(object sender, RoutedEventArgs e)
-        {
-            DescriptionActual.Text = HtmlHelper.HeaderToBold(DescriptionActual.Text);
-        }
-
-        private void BtPremove_Click(object sender, RoutedEventArgs e)
-        {
-            DescriptionActual.Text = HtmlHelper.ParagraphRemove(DescriptionActual.Text);
-        }
-
-
-        private void BtInsertImg_Click(object sender, RoutedEventArgs e)
-        {
-            ImageContextMenu imageContextMenu = ((ImageContextMenu)btAddImgContextMenu.Items[0]);
-
-            string imgUrl = imageContextMenu.ImgUrl;
-            bool imgCent = imageContextMenu.ImgCent;
-            bool imgPx = imageContextMenu.ImgPx;
-            int imgSize = imageContextMenu.ImgSize;
-            bool imgLeft = imageContextMenu.ImgLeft;
-            bool imgCenter = imageContextMenu.ImgCenter;
-            bool imgRight = imageContextMenu.ImgRight;
-
-
-            string imgAdded = "<img src=\"{0}\" style=\"{1}\">";
-            string style = string.Empty;
-            if (!string.IsNullOrEmpty(imgUrl))
-            {
-                if (imgCent && imgSize > 0)
-                {
-                    style += $"width: {imgSize }%;";
-                }
-                else if (imgPx && imgSize > 0)
-                {
-                    style += $"width: {imgSize }px;";
-                }
-
-                if (imgPx || (imgCent && imgSize < 100))
-                {
-                    if (imgRight)
-                    {
-                        imgAdded = "<table style=\"border: 0; width: 100 %;border-spacing: 10px;\"><tr>"
-                             + "<td>" + resources.GetString("LOCDescriptionEditorTextHere") + "</td>"
-                             + $"<td style=\"{style}vertical - align: top;\">"
-                             + imgAdded
-                             + "</td>"
-                             + "</tr></table>";
-                    }
-                    if (imgCenter)
-                    {
-                        imgAdded = "<div style=\"text-align: center;\">"
-                            + imgAdded
-                            + "</div>";
-                    }
-                    if (imgLeft)
-                    {
-                        imgAdded = "<table style=\"border: 0; width: 100 %;border-spacing: 10px;\"><tr>"
-                             + $"<td style=\"{style}vertical-align: top;\">"
-                             + imgAdded
-                             + "</td>"
-                             + "<td>" + resources.GetString("LOCDescriptionEditorTextHere") + "</td>"
-                             + "</tr></table>";
-                    }
-                }
-
-                imgAdded = "\r\n" + HtmlHelper.HtmlFormat(string.Format(imgAdded, imgUrl, style)) + "\r\n";
-
-                if (DescriptionActual.CaretIndex <= 0)
-                {
-                    DescriptionActual.Focus();
-                    DescriptionActual.Text = DescriptionActual.Text.Insert(DescriptionActual.CaretIndex, imgAdded);
-                }
-                else
-                {
-                    DescriptionActual.Text = DescriptionActual.Text.Insert(DescriptionActual.CaretIndex, imgAdded);
-                }
-            }
-
-            btAddImgContextMenu.Visibility = Visibility.Collapsed;
-        }
-
-        private void BtRemoveImg_Click(object sender, RoutedEventArgs e)
-        {
-            DescriptionActual.Text = HtmlHelper.RemoveTag(DescriptionActual.Text, "img");
-        }
-
-        private void Bt100PercentImg_Click(object sender, RoutedEventArgs e)
-        {
-            DescriptionActual.Text = HtmlHelper.Add100PercentStyle(DescriptionActual.Text);
-        }
-
-        private void BtRemoveImgStyle_Click(object sender, RoutedEventArgs e)
-        {
-            DescriptionActual.Text = HtmlHelper.RemoveSizeStyle(DescriptionActual.Text);
-        }
-
-        private void BtCenterImg_Click(object sender, RoutedEventArgs e)
-        {
-            DescriptionActual.Text = HtmlHelper.CenterImage(DescriptionActual.Text);
-        }
-
-
-        private void BtBrtoP_Click(object sender, RoutedEventArgs e)
-        {
-            DescriptionActual.Text = HtmlHelper.BrBrToP(DescriptionActual.Text);
-        }
-
-
-        private void BtBr2to1_Click(object sender, RoutedEventArgs e)
-        {
-            DescriptionActual.Text = HtmlHelper.BrRemove(DescriptionActual.Text, 2, 1);
-        }
-
-        private void BtBr3to1_Click(object sender, RoutedEventArgs e)
-        {
-            DescriptionActual.Text = HtmlHelper.BrRemove(DescriptionActual.Text, 3, 1);
-        }
-
-        private void BtBr3to2_Click(object sender, RoutedEventArgs e)
-        {
-            DescriptionActual.Text = HtmlHelper.BrRemove(DescriptionActual.Text, 3, 2);
-        }
         #endregion
 
+        #region Toolbar Actions (HTML / Markdown)
 
-        #region Steam formatter 
-        private void SteamRemoveAbout_click(object sender, RoutedEventArgs e)
-        {
-            DescriptionActual.Text = HtmlHelper.SteamRemoveAbout(DescriptionActual.Text);
-        }
+        private void BtHtmlFormat_Click(object sender, RoutedEventArgs e) =>
+            DescriptionTextBox.Text = HtmlHelper.HtmlFormat(DescriptionTextBox.Text);
+
+        private void BtHtmlFormatRemove_Click(object sender, RoutedEventArgs e) =>
+            DescriptionTextBox.Text = HtmlHelper.HtmlFormatRemove(DescriptionTextBox.Text);
+
+        private void BtMarkdownToHtml_Click(object sender, RoutedEventArgs e) =>
+            DescriptionTextBox.Text = HtmlHelper.MarkdownToHtml(DescriptionTextBox.Text);
+
+        private void BtHtoB_Click(object sender, RoutedEventArgs e) =>
+            DescriptionTextBox.Text = HtmlHelper.HeaderToBold(DescriptionTextBox.Text);
+
+        private void BtPremove_Click(object sender, RoutedEventArgs e) =>
+            DescriptionTextBox.Text = HtmlHelper.ParagraphRemove(DescriptionTextBox.Text);
+
+        private void BtBrtoP_Click(object sender, RoutedEventArgs e) =>
+            DescriptionTextBox.Text = HtmlHelper.BrBrToP(DescriptionTextBox.Text);
+
+        private void BtBr2to1_Click(object sender, RoutedEventArgs e) =>
+            DescriptionTextBox.Text = HtmlHelper.BrRemove(DescriptionTextBox.Text, 2, 1);
+
+        private void BtBr3to1_Click(object sender, RoutedEventArgs e) =>
+            DescriptionTextBox.Text = HtmlHelper.BrRemove(DescriptionTextBox.Text, 3, 1);
+
+        private void BtBr3to2_Click(object sender, RoutedEventArgs e) =>
+            DescriptionTextBox.Text = HtmlHelper.BrRemove(DescriptionTextBox.Text, 3, 2);
+
+        private void SteamRemoveAbout_click(object sender, RoutedEventArgs e) =>
+            DescriptionTextBox.Text = HtmlHelper.SteamRemoveAbout(DescriptionTextBox.Text);
+
         #endregion
 
-
-        private void DescriptionActual_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            htmlTextView.HtmlText = ((TextBox)sender).Text;
-
-            if (!DisableEvent)
-            {
-                if (ListUndo.Count == 0)
-                {
-                    ListUndo.Add(((TextBox)sender).Text);
-                }
-                else if (IndexUndo == ListUndo.Count - 1)
-                {
-                    ListUndo.Add(((TextBox)sender).Text);
-                }
-                else
-                {
-                    ListUndo.RemoveRange(IndexUndo, ListUndo.Count - 1 - IndexUndo);
-                    ListUndo.Add(((TextBox)sender).Text);
-                }
-
-                IndexUndo = ListUndo.Count - 1;
-
-                if (ListUndo.Count > 1)
-                {
-                    PART_Undo.IsEnabled = true;
-                    PART_Redo.IsEnabled = false;
-                }
-            }
-            else
-            {
-                DisableEvent = false;
-            }
-        }
-
+        #region Image Actions
 
         private void BtAddImg_Click(object sender, RoutedEventArgs e)
         {
             btAddImgContextMenu.Visibility = Visibility.Visible;
         }
 
+        private void BtInsertImg_Click(object sender, RoutedEventArgs e)
+        {
+            var imageContextMenu = (ImageContextMenu)btAddImgContextMenu.Items[0];
+            if (string.IsNullOrEmpty(imageContextMenu.ImgUrl)) return;
 
-        #region Undo/Redo
+            string imgHtml = BuildImageHtml(imageContextMenu.ImgUrl, imageContextMenu);
+
+            int insertPos = DescriptionTextBox.CaretIndex;
+            _ = DescriptionTextBox.Focus();
+            DescriptionTextBox.Text = DescriptionTextBox.Text.Insert(insertPos, "\r\n" + imgHtml + "\r\n");
+
+            btAddImgContextMenu.Visibility = Visibility.Collapsed;
+        }
+
+        private string BuildImageHtml(string url, ImageContextMenu config)
+        {
+            string style = string.Empty;
+            if (config.ImgCent && config.ImgSize > 0)
+                style += $"width:{config.ImgSize}%;";
+            else if (config.ImgPx && config.ImgSize > 0)
+                style += $"width:{config.ImgSize}px;";
+
+            string imgTag = $"<img src=\"{url}\" style=\"{style}\">";
+
+            if (config.ImgCenter) imgTag = $"<div style=\"text-align:center;\">{imgTag}</div>";
+
+            if (config.ImgLeft || config.ImgRight)
+            {
+                string tdLeft = config.ImgLeft ? imgTag : resources.GetString("LOCDescriptionEditorTextHere");
+                string tdRight = config.ImgRight ? imgTag : resources.GetString("LOCDescriptionEditorTextHere");
+
+                imgTag = $"<table style=\"border:0;width:100%;border-spacing:10px;\"><tr><td>{tdLeft}</td><td>{tdRight}</td></tr></table>";
+            }
+
+            return HtmlHelper.HtmlFormat(imgTag);
+        }
+
+        private void BtRemoveImg_Click(object sender, RoutedEventArgs e) =>
+            DescriptionTextBox.Text = HtmlHelper.RemoveTag(DescriptionTextBox.Text, "img");
+
+        private void Bt100PercentImg_Click(object sender, RoutedEventArgs e) =>
+            DescriptionTextBox.Text = HtmlHelper.Add100PercentStyle(DescriptionTextBox.Text);
+
+        private void BtRemoveImgStyle_Click(object sender, RoutedEventArgs e) =>
+            DescriptionTextBox.Text = HtmlHelper.RemoveSizeStyle(DescriptionTextBox.Text);
+
+        private void BtCenterImg_Click(object sender, RoutedEventArgs e) =>
+            DescriptionTextBox.Text = HtmlHelper.CenterImage(DescriptionTextBox.Text);
+
+        #endregion
+
+        #region Undo / Redo
+
+        private void DescriptionTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            htmlTextView.HtmlText = DescriptionTextBox.Text;
+
+            if (DisableEvent) { DisableEvent = false; return; }
+
+            AddToUndo(DescriptionTextBox.Text);
+
+            PART_Undo.IsEnabled = ListUndo.Count > 1;
+            PART_Redo.IsEnabled = false;
+        }
+
+        private void AddToUndo(string text)
+        {
+            if (ListUndo.Count == 0 || IndexUndo == ListUndo.Count - 1)
+            {
+                ListUndo.Add(text);
+            }
+            else
+            {
+                ListUndo.RemoveRange(IndexUndo + 1, ListUndo.Count - IndexUndo - 1);
+                ListUndo.Add(text);
+            }
+            IndexUndo = ListUndo.Count - 1;
+        }
+
         private void PART_Undo_Click(object sender, RoutedEventArgs e)
         {
-            DisableEvent = true;
+            if (IndexUndo <= 0) return;
 
-            IndexUndo = IndexUndo - 1;
-            DescriptionActual.Text = ListUndo[IndexUndo];
+            DisableEvent = true;
+            IndexUndo--;
+            DescriptionTextBox.Text = ListUndo[IndexUndo];
 
             PART_Redo.IsEnabled = true;
-            if (IndexUndo == 0)
-            {
-                PART_Undo.IsEnabled = false;
-            }
+            PART_Undo.IsEnabled = IndexUndo > 0;
         }
 
         private void PART_Redo_Click(object sender, RoutedEventArgs e)
         {
-            DisableEvent = true;
+            if (IndexUndo >= ListUndo.Count - 1) return;
 
-            IndexUndo = IndexUndo + 1;
-            DescriptionActual.Text = ListUndo[IndexUndo];
+            DisableEvent = true;
+            IndexUndo++;
+            DescriptionTextBox.Text = ListUndo[IndexUndo];
 
             PART_Undo.IsEnabled = true;
-            if (IndexUndo == ListUndo.Count - 1)
-            {
-                PART_Redo.IsEnabled = false;
-            }
+            PART_Redo.IsEnabled = IndexUndo < ListUndo.Count - 1;
         }
 
-        private void DescriptionActual_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        private void DescriptionTextBox_PreviewKeyDown(object sender, KeyEventArgs e)
         {
-            if (e.Key == Key.Z && (e.KeyboardDevice.Modifiers & ModifierKeys.Control) != 0)
+            if ((Keyboard.Modifiers & ModifierKeys.Control) != 0)
             {
-                if (PART_Undo.IsEnabled)
-                {
-                    PART_Undo_Click(null, null);
-                }
-                e.Handled = true;
-            }
-
-            if (e.Key == Key.Y && (e.KeyboardDevice.Modifiers & ModifierKeys.Control) != 0)
-            {
-                if (PART_Redo.IsEnabled)
-                {
-                    PART_Redo_Click(null, null);
-                }
-                e.Handled = true;
+                if (e.Key == Key.Z && PART_Undo.IsEnabled) { PART_Undo_Click(null, null); e.Handled = true; }
+                if (e.Key == Key.Y && PART_Redo.IsEnabled) { PART_Redo_Click(null, null); e.Handled = true; }
             }
         }
+
         #endregion
 
+        #region Selection Highlight
 
-        private void DescriptionActual_SelectionChanged(object sender, RoutedEventArgs e)
+        private void DescriptionTextBox_SelectionChanged(object sender, RoutedEventArgs e)
         {
-            htmlTextView.HtmlText = DescriptionActual.Text;
+            htmlTextView.HtmlText = DescriptionTextBox.Text;
 
-            string sSelectedText = DescriptionActual.SelectedText;
-            if (!sSelectedText.IsNullOrEmpty())
+            string selected = DescriptionTextBox.SelectedText;
+            if (string.IsNullOrEmpty(selected)) return;
+
+            int start = Math.Max(DescriptionTextBox.SelectionStart - 1, 0);
+            int length = DescriptionTextBox.SelectionLength + 2;
+            length = Math.Min(length, htmlTextView.HtmlText.Length - start);
+
+            string extended = htmlTextView.HtmlText.Substring(start, length);
+            if (!Regex.IsMatch(extended, @"^.?<") && !Regex.IsMatch(extended, @">.?$") && !Regex.IsMatch(extended, @"\/.?$"))
             {
-                int posStart = DescriptionActual.SelectionStart;
-                int length = DescriptionActual.SelectionLength;
-
-                if (posStart + length + 1 < DescriptionActual.Text.Length)
-                {
-                    length++;
-                }
-                if (posStart > 1)
-                {
-                    posStart--;
-                    length++;
-                }
-
-                string sSelectedTextExtend = htmlTextView.HtmlText.Substring(posStart, length);
-                if (!Regex.IsMatch(sSelectedTextExtend, @"^.?<") && !Regex.IsMatch(sSelectedTextExtend, @">.?$") && !Regex.IsMatch(sSelectedTextExtend, @"\/.?$"))
-                {
-                    htmlTextView.HtmlText = htmlTextView.HtmlText.Replace(sSelectedText, "<span style=\"background-color: yellow; color: black;\">" + sSelectedText + "</span>");
-                }
+                htmlTextView.HtmlText = htmlTextView.HtmlText.Replace(selected, $"<span style=\"background-color: yellow; color: black;\">{selected}</span>");
             }
         }
+
+        #endregion
     }
 }
